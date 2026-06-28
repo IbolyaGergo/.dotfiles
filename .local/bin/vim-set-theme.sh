9bf5c9fdf0fa79e84c8e6e7a8515c353656ff1a7
IbolyaGergo <ibolyagergi@gmail.com>
2026-06-28 18:34:21 +0200

    refactor(vim): remove remote send loop
    It could not be used to update the background of running 'vim' instances
    using 'systemd'.

--- .local/bin/vim-set-theme.sh
+++ .local/bin/vim-set-theme.sh
@@ -10,13 +10,3 @@ if [[ "$THEME" != "light" && "$THEME" != "dark" ]]; then
 fi
 
 echo "set background=$THEME" > "$HOME/.vim/theme.vim"
-
-# Iterate over each Vim server listed.
-# We use 'while read' instead of a 'for' loop to ensure that if a server name
-# contains spaces, it is treated as a single entry. A 'for' loop would incorrectly
-# split names by whitespace, leading to errors.
-while read -r server; do
-    [[ -z "$server" ]] && continue
-
-    vim --servername $server --remote-send ":set background=$THEME<CR>"
-done <<< "$(vim --serverlist)"
